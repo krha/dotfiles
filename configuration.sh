307030a21e04a881a0abcf13892c00fe2826659a
Kiryong Ha <krha@cmu.edu>
2015-07-30 16:51:44 -0400

    add git initial config

--- configuration.sh
+++ configuration.sh
@@ -5,8 +5,11 @@
 # ref: http://askubuntu.com/questions/363346/how-to-permanently-switch-caps-lock-and-esc
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
-# git diff using vimdiff
-# ref: http://stackoverflow.com/questions/3713765/viewing-all-git-diffs-with-vimdiff
+# git configuration
+git config --global user.name "Kiryong Ha"
+git config --global user.email "krha@cmu.edu"
+
+# git diff using vimdiff (ref: http://stackoverflow.com/questions/3713765/viewing-all-git-diffs-with-vimdiff)
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
 git config --global alias.d difftool
